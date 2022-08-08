c8011b1a6c88fdee265a7c04276f37f32d2b19aa
ManthanKumar1 <80151387+ManthanKumar1@users.noreply.github.com>
2022-08-08 17:41:32 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,6 +8,20 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
+router.get('/cohort-members', function(req,res){
+    let students = ['abc','mno','xyz']
+    console.log(students)
+    res.send("hi")
+})
+
+router.get('/student-details/:name/:age', function(req,res){
+    console.log("This is the request "+ JSON.stringify(req.params))
+    let reqParams = req.params
+    let studentName = reqParams.name
+    console.log('Name of the student is ', studentName)
+    let student = studentName + " " + studentName
+    res.send("studentDetails")
+})
 
 router.get('/test-you', function(req, res){
     res.send('This is the second routes implementation')
